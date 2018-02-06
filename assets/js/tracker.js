dad7551cee0209972bde2031b5a0c91cbb4ed876
tdball <trevor.d.ball@gmail.com>
2018-02-06 12:54:13 -0600

    moved initMap() to it's own js file, loading custom JS in <head> now instead of defining in <script> tags at the bottom of the body

---
+++ assets/js/tracker.js
@@ -0,0 +1,8 @@
+// Callback for Google Maps API
+function initMap() {
+    let gmap = new google.maps.Map(document.getElementById('gmap'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    })
+}
+
