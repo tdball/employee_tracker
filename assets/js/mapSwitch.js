2addc7042c5ce114937e4b09f434563e9b83479e
tdball <trevor.d.ball@gmail.com>
2018-02-06 14:33:59 -0600

    started using a class to store data about the map, defaults to google, switches to leaflet on toggle.

--- assets/js/mapSwitch.js
+++ assets/js/mapSwitch.js
@@ -1,14 +1,33 @@
+class MapSwitch {
+    /*
+    Data structure to hold map state. Allows for toggling between two providers
+    without incurring further page loads
+    */
 
-function toggleMaps() {
-    googleMapActive = !googleMapActive
-    let openMap = document.getElementById('omap')
-    let googleMap = document.getElementBy('id')
-    if(google) {
-        openMap.style.display = 'hidden'
-        googleMap.style.display = 'block'
-    } else {
-        openMap.style.display = 'block'
-        googleMap.style.display = 'hidden'
+    constructor() {
+        this.googleMapActive = true
     }
+
+    toggleMaps() {
+        /*
+        Retrieves the active state of the MapSwitch object and swaps between either
+        leaflet or googlemaps as a provider for the Map
+         */
+        console.log(this.googleMapActive)
+        this.googleMapActive = !this.googleMapActive
+        let openMap = document.getElementById('omap')
+        let googleMap = document.getElementById('gmap')
+        if(this.googleMapActive) {
+            this.provider = 'google'
+            openMap.style.display = 'none'
+            googleMap.style.display = 'block'
+        } else {
+            this.provider = 'leaflet'
+            openMap.style.display = 'block'
+            googleMap.style.display = 'none'
+        }
+    }
+
 }
+
 
