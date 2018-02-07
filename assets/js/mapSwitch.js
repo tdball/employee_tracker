cd70ec914c2bcce414ff12f7f6fd48d30231e430
tdball <trevor.d.ball@gmail.com>
2018-02-07 09:51:08 -0600

    hacky solution in place for mapResizing on then omap map. Custom event, combined with a 1ms timeout.

--- assets/js/mapSwitch.js
+++ assets/js/mapSwitch.js
@@ -7,6 +7,18 @@ class MapSwitch {
     constructor() {
         this.googleMapActive = true
         this.toggleButton = document.getElementById('mapToggle')
+        this.mapList = []
+        this.mapResize = new CustomEvent("mapResize")
+        document.addEventListener("mapResize", this.resizeMaps)
+    }
+
+    resizeMaps() {
+        // Hacky solution, due to display:none being the default for the leaflet map on load
+        // this timeout of 1ms will let the browser swap the display to block and then the
+        // size of the map will be adjusted to take up the needed space.
+        setTimeout(function() {
+            document.getElementById('omap').leaflet.invalidateSize()
+        }, 1)
     }
 
     toggleMaps() {
@@ -14,9 +26,12 @@ class MapSwitch {
         Retrieves the active state of the MapSwitch object and swaps between either
         leaflet or googlemaps as a provider for the Map
          */
+        document.dispatchEvent(this.mapResize)
         this.googleMapActive = !this.googleMapActive
+
         let openMap = document.getElementById('omap')
         let googleMap = document.getElementById('gmap')
+
         if(this.googleMapActive) {
             this.provider = 'google'
             this.toggleButton.innerText = 'Switch To Leaflet'
