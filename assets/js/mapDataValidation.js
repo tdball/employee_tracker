6cf437bf5e9cae1a3d5660ac9bea4b5e2a81660b
tdball <trevor.d.ball@gmail.com>
2018-02-06 22:31:12 -0600

    added click listener to markers, displays name above the marker, only on google for now

--- assets/js/mapDataValidation.js
+++ assets/js/mapDataValidation.js
@@ -2,7 +2,7 @@ function validateCoordinates(coordinateObject) {
     // Quick and dirty validation, needs improvement once I've learned how to determine
     // If a location is water or not.
     // TODO: Revise this method, just a placeholder for now
-    if(!coordinateObject.latitude || !coordinateObject.longitude || !coordinateObject.logo) {
+    if(!coordinateObject.latitude || !coordinateObject.longitude) {
         return false
     } else {
         return true
