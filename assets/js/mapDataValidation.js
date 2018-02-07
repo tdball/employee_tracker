817fde294dd9802fed562752aa9dd57289fc8d27
tdball <trevor.d.ball@gmail.com>
2018-02-06 22:12:13 -0600

    added markers to google maps

---
+++ assets/js/mapDataValidation.js
@@ -0,0 +1,10 @@
+function validateCoordinates(coordinateObject) {
+    // Quick and dirty validation, needs improvement once I've learned how to determine
+    // If a location is water or not.
+    // TODO: Revise this method, just a placeholder for now
+    if(!coordinateObject.latitude || !coordinateObject.longitude || !coordinateObject.logo) {
+        return false
+    } else {
+        return true
+    }
+}
