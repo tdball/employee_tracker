74995c773737a2624ff99d1584de7f588214b529
tdball <trevor.d.ball@gmail.com>
2018-02-08 21:35:43 -0600

    fixed server side validation, wrong direction on my less than or equal to's

--- api/geo_scrape_api.py
+++ api/geo_scrape_api.py
@@ -24,9 +24,11 @@ def in_water_view():
         latitude = request.args.get('lat')
         longitude = request.args.get('lng')
         if latitude and longitude:
-            if -90 >= latitude <= 90:
-                if -180 >= longitude <= 90:
-                    return json.dumps({"inWater": in_water(float(latitude), float(longitude))})
+            latitude = float(latitude)
+            longitude = float(longitude)
+            if -90 <= latitude <= 90:
+                if -180 <= longitude <= 90:
+                    return json.dumps({"inWater": in_water(latitude, longitude)})
                 else:
                     return json.dumps({"invalidData": "`lng` out of range."})
             else:
